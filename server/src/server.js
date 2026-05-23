b67c81a4dea498a66feba377269be03c9229174d
Imran1692 <imranhossain386434@gmail.com>
2026-05-23 13:24:45 +0600

    db connected to server

---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import connectDB from './db/index.js'
+import app from './app.js'
+
+connectDB()
+
+.then(() => {
+    app.listen(process.env.PORT || 5001,() => {
+        console.log(
+            `server is running at port:${process.env.PORT}`
+        );
+    })
+})
+.catch((err) => {console.log("mongodb connection faild",err);
+})
